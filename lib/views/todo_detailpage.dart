cbd3e595a02b7c903945404cda5475c566e60167
AdreshSingh <adreshsingh271527@gmail.com>
2024-12-15 04:38:44 +0530

    revamped the detail page

--- lib/views/todo_detailpage.dart
+++ lib/views/todo_detailpage.dart
@@ -1,103 +1,126 @@
+import 'package:do_it_cutely2/domain/model/todo.dart';
 import 'package:flutter/material.dart';
 
-class TodoDetailpage extends StatefulWidget {
-  const TodoDetailpage({super.key});
+class TodoDetailpage extends StatelessWidget {
+  const TodoDetailpage({super.key, required this.todo});
 
-  @override
-  State<TodoDetailpage> createState() => _TodoDetailpageState();
-}
+  final Todo? todo;
 
-class _TodoDetailpageState extends State<TodoDetailpage> {
+  final String title = """
+Create todos, you see them in detail, and make a lot of them. 
+ 1. double tap to complete todos.
+ 2. single to see them in details.
+ 3. Goodluck 🍀+ 🔥 
+ ---------------- 
+ While on cleaning up: 
+ 1. press single one tap to see in detail.
+ 2. long press on a single todo makes it delete forever.
+ Once gone, Never come back,
+ There are still bugs but if u encounter, happy to say it to me....
+ Arpit 
+ email: arpit20096063242@gmail.com """;
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Expanded(
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/cute_board.png"),
-                fit: BoxFit.fill,
+    Todo backupTodo = Todo(
+        id: 1734138298662278,
+        title: "Help Here! 🔥",
+        text: title,
+        isDone: true);
+    backupTodo = todo ?? backupTodo;
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: (Colors.red[300])!.withOpacity(.27),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 12,
+                    left: 8.0,
+                    bottom: 8.0,
+                    right: 8.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //? title
+                      SingleChildScrollView(
+                        child: Text(
+                          backupTodo.title,
+                          style: TextStyle(
+                            color: Colors.red[100],
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      //gaps
+                      const SizedBox(height: 10),
+
+                      // ? date
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          "${DateTime.fromMicrosecondsSinceEpoch(backupTodo.id).toLocal()}",
+                          style: TextStyle(
+                            color: Colors.red[100],
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+
+                      // gaps
+                      const SizedBox(height: 10),
+                      //? content
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Text(
+                            backupTodo.text,
+                            style: TextStyle(
+                              color: Colors.red[100],
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            margin: const EdgeInsets.only(
-              bottom: 35,
-              right: 30,
-              left: 10,
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 15,
-            ),
-            width: 200,
-            height: 240,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              gradient: const LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                  Color.fromARGB(243, 224, 224, 224),
-                  Color.fromARGB(245, 182, 182, 182),
-                ],
+          //? completed or incompleted
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red[300],
               ),
-              boxShadow: const [
-                BoxShadow(
-                  offset: Offset(4.0, 4.0),
-                  blurRadius: 15.0,
-                  color: Color.fromARGB(130, 0, 0, 0),
-                  spreadRadius: 1.0,
+              child: Text(
+                backupTodo.isDone ? "<- Completed" : "<- Incomplete",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
                 ),
-                BoxShadow(
-                    offset: Offset(-4.0, -4.0),
-                    blurRadius: 10.0,
-                    color: Colors.grey,
-                    spreadRadius: 1.0)
-              ],
-            ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // header
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // title
-                    Text(
-                      "Welcome",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20,
-                      ),
-                    ),
-
-                    // date
-                    Text(
-                      "29-july-2024",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 12,
-                      ),
-                    )
-                  ],
-                ),
-
-                SizedBox(
-                  height: 10,
-                ),
-
-                // text
-                Text("I am creating cute app"),
-              ],
+              ),
             ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
